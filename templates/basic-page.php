39238f0d8094175cfd83d885083104f3c9b70164
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-04-12 07:25:54 +0200

    cleanup

--- templates/basic-page.php
+++ templates/basic-page.php
@@ -1,13 +1,1 @@
-<?php namespace ProcessWire;
-
-// $url = 'https://processwire.com/blog/posts/processwire-3.0.62-and-more-on-markup-regions/';
-?>
-
-<!-- <div data-pw-prepend="content-body">
-
-	<h1>
-		This Is Basic Page Template.
-    More about <a href='<?php // echo $url ?>'>markup regions</a>
-	</h1>
-
-</div> -->
+<?php namespace ProcessWire; ?>
